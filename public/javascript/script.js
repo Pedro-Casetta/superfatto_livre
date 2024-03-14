f63cd9ad20e3a14cd5cd4cb7ca7043a0e8c840e7
Pedro-Casetta <111102537+Pedro-Casetta@users.noreply.github.com>
2024-03-14 13:57:07 -0300

    Add files via upload

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -10,17 +10,21 @@ function exibirCampoCadastroConta($tipo) {
         document.getElementById('credencial').setAttribute('required', '');
         document.getElementById('campoTelefone').style.display = "none";
         document.getElementById('telefone').removeAttribute('required');
+        document.getElementById('telefone').value = "";
 
     } else if ($tipo == "cliente") {
         document.getElementById('campoCredencial').style.display = "none";
         document.getElementById('credencial').removeAttribute('required');
+        document.getElementById('credencial').value = "";
         document.getElementById('campoTelefone').style.display = "inline";
         document.getElementById('telefone').setAttribute('required', '');
     } else {
         document.getElementById('campoTelefone').style.display = "none";
         document.getElementById('campoCredencial').style.display = "none";
         document.getElementById('credencial').removeAttribute('required');
+        document.getElementById('credencial').value = "";
         document.getElementById('telefone').removeAttribute('required');
+        document.getElementById('telefone').value = "";
     }
 }
 
@@ -44,3 +48,41 @@ function alterarQuantidadeProdutoCarrinho(id, app_host, produtoId) {
         window.location.reload();
     }, 1500);
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+    let botoes_editar = document.getElementsByClassName("editar");
+    let botoes_excluir = document.getElementsByClassName("excluir");
+
+    for (let i = 0; i < botoes_editar.length; i++) {
+        let botao_editar = botoes_editar[i].getBoundingClientRect();
+        let botao_excluir = botoes_excluir[i].getBoundingClientRect();
+
+        if (botao_editar.bottom <= botao_excluir.top)
+        {
+            botoes_editar[i].style.marginBottom = "10px";
+        }
+        else
+        {
+            botoes_editar[i].style.marginBottom = "";
+        }
+    }
+});
+
+window.addEventListener("resize", () => {
+    var botoes_editar = document.getElementsByClassName("editar");
+    var botoes_excluir = document.getElementsByClassName("excluir");
+
+    for (let i = 0; i < botoes_editar.length; i++) {
+        var botao_editar = botoes_editar[i].getBoundingClientRect();
+        var botao_excluir = botoes_excluir[i].getBoundingClientRect();
+
+        if (botao_editar.bottom <= botao_excluir.top)
+        {
+            botoes_editar[i].style.marginBottom = "10px";
+        }
+        else
+        {
+            botoes_editar[i].style.marginBottom = "";
+        }
+    }
+});
